4f7acaaed1bae110cc9e828d3310e25079a7b096
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2020-01-19 00:59:26 +0300

    Tugas 10 PHP

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$ayamgoreng = 13000;
+$ayambakar = 15000;
+$ayamsayur = 13000;
+$tempegoreng = 1000;
+$tahugoreng = 1000;
+$soto = 3000;
+$nasi = 5000;
+$esteh = 4000;
+$estebu = 5000;
+
+print ("<h1>Daftar Menu</h1>");
+echo "<pre>";
+echo "• &ensp;Ayam Goreng   = Rp. $ayamgoreng <br>";
+echo "• &ensp;Ayam Bakar    = Rp. $ayambakar <br>";
+echo "• &ensp;Ayam Sayur    = Rp. $ayamsayur <br>";
+echo "• &ensp;Tempe Goreng  = Rp. $tempegoreng <br>";
+echo "• &ensp;Tahu Goreng   = Rp. $tahugoreng <br>";
+echo "• &ensp;Soto          = Rp. $soto <br>";
+echo "• &ensp;Nasi Putih    = Rp. $nasi <br>";
+echo "• &ensp;Es Teh        = Rp. $esteh <br>";
+echo "• &ensp;Es Tebu       = Rp. $estebu <br>";
+echo "</pre>";
+
+
+?>
